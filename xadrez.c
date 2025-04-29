10ccbebe8c7e5c72335de8c3869a630ea2131181
DELTAMIKE58 <moraisdiogo582@gmail.com>
2025-04-29 12:34:05 +0000

    cavalo pronto

--- xadrez.c
+++ xadrez.c
@@ -21,8 +21,25 @@ int rainha = 1;
     do {
     printf("esquerda\n", rainha);
         rainha++;
-    } while (rainha <= 8);
+     } while (rainha <= 8);
    
+
+
+    int cavalo = 1 ;
+    while (cavalo--)
+    {  
+        for (int i = 0; i < 2 ; i++){  
+             printf("cima\n");
+         }
+             printf("direita\n");
+
+    }
+
+
+
+
+
+
 
     return 0;
 }
